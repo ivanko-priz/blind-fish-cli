10264c90ef2b214adb69a4fba9077ba447144f5b
Ivan Prizov <ivanko.priz@gamil.com>
2021-07-16 16:55:52 +0300

    Update App class to include subcommands

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,33 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.mycompany.app.commands.BlindFishSetup;
+import com.mycompany.app.commands.BlindFishStart;
+
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(
+    name = "blind-fish",
+    version = "blind-fish 1.0",
+    description = "Play blindfold chess against stockfish engine",
+    subcommands = {
+        BlindFishSetup.class,
+        BlindFishStart.class,
+    }
+)
+public class App implements Callable<Integer>
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }  
+
+    @Override
+    public Integer call() {
+        System.out.println("Blind-fish. Play blindfold chess against stockfish engine.");
+        return 0;
     }
 }
